7303aa44e525e163d2fe6bb7bcdfc981b99f6056
waseem-avi <waseem.riyaz@avisoft.io>
2024-02-29 18:46:10 +0530

    modified homepage

--- djanQl/homepage/views.py
+++ djanQl/homepage/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from student.models import Student
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    # return HttpResponse("<h1>Hello World!</h1>")
-    return render(request, "home.html", {})
+    context = {}
+    datalist = Student.objects.all()
+    context['data'] = datalist
+    return render(request, "home.html", context)
 
 def context_view(request , *args, **kwargs):
     my_context = {
